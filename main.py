b2de80cf9ce2c4018c17b1434bfd41ee397eb004
harmith-singh <harmeet.rpds@gmail.com>
2022-04-29 10:53:00 +0530

    Started making the blog using existing template (cwh-vid6)

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
 @app.route("/")
-def main():
+def home():
     return render_template('index.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
 
 
 @app.route("/about")
 def about():
-    person = 'Harmith'
-    return render_template('about.html', name=person)
-
-
-@app.route("/bootstrap")
-def bootstrap():
-    return render_template('bootstrap_sample.html')
+    return render_template('about.html')
 
 
 app.run(debug=True)
